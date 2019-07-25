4c3474108076b78f7711820d459a07f93a1563a0
Pedro Enrique Palau <pepalauisaac@gmail.com>
2019-07-25 14:41:06 -0500

    Remove extra spaces

--- acf-menu-chooser.php
+++ acf-menu-chooser.php
@@ -13,11 +13,9 @@
  * GitHub Branch:     master
 */
 
-
 // 1. set text domain
 // Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
 load_plugin_textdomain( 'acf-menu-chooser', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
-
 
 // 2. Include field type for ACF5
 // $version = 5 and can be ignored until ACF6 exists
@@ -26,4 +24,3 @@ function include_field_types_menu_chooser( $version ) {
 }
 
 add_action('acf/include_field_types', 'include_field_types_menu_chooser');
-?>
